91cfb74c631ce2c354e55b2026219e392d67917f
Alexis Algoud <alexis@atm-consulting.fr>
2015-07-27 10:12:20 +0200

    FIX sérieux, faut lire les classes

--- tpl/tasklist.onglet.workstations.php
+++ tpl/tasklist.onglet.workstations.php
@@ -3,15 +3,8 @@
 	//Récupération de la liste des postes de travail
 	$disabled = ($user->rights->workstation->all->lire) ? 0 : 1;
 	
-	$sql = "SELECT rowid, name FROM ".MAIN_DB_PREFIX."workstation";
-	$PDOdb->Execute($sql);
-	$TWorkstationTemp = $PDOdb->Get_All(PDO::FETCH_ASSOC);
-	
-	$TWorkstation[-1] = 'Tous';
-	foreach($TWorkstationTemp as $workstation){
-		$TWorkstation[$workstation['rowid']] = $workstation['name'];
-	}
-	
+	$TWorkstation = array_merge( array(-1=>'Tous') , TWorkstation::getWorstations($PDOdb));
+    
 	//pre($TWorkstation,true);exit;
 	$form = new TFormCore;
 	$selectWorkstation = $form->combo('','search_workstation',$TWorkstation,'',1,'','data-native-menu="false"');
