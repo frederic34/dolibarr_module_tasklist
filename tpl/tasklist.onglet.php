1ce3c0a51362e41c8ed62a60513bf5c72ddd4ac7
Alexis Algoud <alexis@atm-consulting.fr>
2016-02-06 18:56:54 +0100

    FIX nav

--- tpl/tasklist.onglet.php
+++ tpl/tasklist.onglet.php
@@ -1,47 +1,58 @@
 <nav class="navbar navbar-default navbar-fixed-top">
-<div class="collapse navbar-collapse " id="menu-tasklist">
-    <ul class="nav nav-tabs navbar-nav" role="tablist">
-	  <li class="active"><a href="#list-task-user" role="tab" data-toggle="tab"><?php echo $langs->trans('Users'); ?></a></li>
-	  <?php if($conf->workstation->enabled && $user->rights->workstation->all->read){ ?><li><a href="#list-task-workstation" id="onglet2"  role="tab" data-toggle="tab"><?php echo $langs->trans('WorkStations'); ?></a></li><?php } ?>
-      <?php if($accessOF) { ?><li><a href="#list-of" id="onglet3" role="tab" data-toggle="tab"><?php echo $langs->trans('OFAsset'); ?></a></li><?php } ?>
-	</ul>
-	
-	<ul class="nav navbar-nav navbar-right">	
-			<li>
-<?php
-if($user->rights->tasklist->user->read) {
-?>
-				<div class="button-group">
-			        <a type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> <span id="user-name"><?php echo $user->login ?></span> <span class="caret"></span></a>
-					<input id="search_user" type="hidden" value="<?php echo $user->id ?>" />
-					
-						<ul class="dropdown-menu" id="select-user-list" style="height: 500px; overflow-y: scroll; ">
-<?php
-	global $conf;
-	
-	$sql = "SELECT DISTINCT u.rowid,u.login
-			FROM ".MAIN_DB_PREFIX."user as u
-			LEFT JOIN ".MAIN_DB_PREFIX."usergroup_user as uu ON (uu.fk_user = u.rowid)
-			WHERE u.statut = 1 AND u.entity IN (0,".$conf->entity.")
-			ORDER BY login";
-	
-	$TUser = $PDOdb->ExecuteAsArray($sql);
-	foreach($TUser as $obj) {
-		echo '<li class="btn" login="'.$obj->login.'" user-id="'.$obj->rowid.'"><a href="javascript:changeUser('.$obj->rowid.')">'. $obj->login .'</a></li>';	
-	}
-
-?>
-						</ul>
-				
-				</div>
-<?php
-}
-else {
-	echo '<p class="navbar-text navbar-right"><span class="glyphicon glyphicon-user"></span> <span id="user-name">'.$user->login.'&nbsp;</span></p>';
-}
-?>				
-
-			</li>
-	</ul>
-</div>
+	<div class="container-fluid">
+		<div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu-tasklist" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	    </div>
+			
+		<div class="collapse navbar-collapse " id="menu-tasklist">
+		    <ul class="nav nav-tabs navbar-nav" role="tablist">
+			  <li class="active"><a href="#list-task-user" role="tab" data-toggle="tab"><?php echo $langs->trans('Users'); ?></a></li>
+			  <?php if($conf->workstation->enabled && $user->rights->workstation->all->read){ ?><li><a href="#list-task-workstation" id="onglet2"  role="tab" data-toggle="tab"><?php echo $langs->trans('WorkStations'); ?></a></li><?php } ?>
+		      <?php if($accessOF) { ?><li><a href="#list-of" id="onglet3" role="tab" data-toggle="tab"><?php echo $langs->trans('OFAsset'); ?></a></li><?php } ?>
+			</ul>
+			
+			<ul class="nav navbar-nav navbar-right">	
+					<li>
+		<?php
+		if($user->rights->tasklist->user->read) {
+		?>
+						<div class="button-group">
+					        <a type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> <span id="user-name"><?php echo $user->login ?></span> <span class="caret"></span></a>
+							<input id="search_user" type="hidden" value="<?php echo $user->id ?>" />
+							
+								<ul class="dropdown-menu" id="select-user-list" style="height: 500px; overflow-y: scroll; ">
+		<?php
+			global $conf;
+			
+			$sql = "SELECT DISTINCT u.rowid,u.login
+					FROM ".MAIN_DB_PREFIX."user as u
+					LEFT JOIN ".MAIN_DB_PREFIX."usergroup_user as uu ON (uu.fk_user = u.rowid)
+					WHERE u.statut = 1 AND u.entity IN (0,".$conf->entity.")
+					ORDER BY login";
+			
+			$TUser = $PDOdb->ExecuteAsArray($sql);
+			foreach($TUser as $obj) {
+				echo '<li class="btn" login="'.$obj->login.'" user-id="'.$obj->rowid.'"><a href="javascript:changeUser('.$obj->rowid.')">'. $obj->login .'</a></li>';	
+			}
+		
+		?>
+								</ul>
+						
+						</div>
+		<?php
+		}
+		else {
+			echo '<p class="navbar-text navbar-right"><span class="glyphicon glyphicon-user"></span> <span id="user-name">'.$user->login.'&nbsp;</span></p>';
+		}
+		?>				
+		
+					</li>
+			</ul>
+		</div>
+	</div>
 </nav>
