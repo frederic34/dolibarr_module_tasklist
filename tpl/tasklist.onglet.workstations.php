168dd0d2c6777c9f1e78aaae9a0d83d806c62d2c
gauthier <gauthier.verdol@atm-consulting.fr>
2017-11-23 12:47:34 +0100

    FIX : array_merge ne conserve pas les clefs

--- tpl/tasklist.onglet.workstations.php
+++ tpl/tasklist.onglet.workstations.php
@@ -3,7 +3,7 @@
 	//Récupération de la liste des postes de travail
 	$disabled = ($user->rights->workstation->all->lire) ? 0 : 1;
 	
-	$TWorkstation = array_merge( array(-1=>'Tous') , TWorkstation::getWorstations($PDOdb));
+	$TWorkstation = array(-1=>'Tous') + TWorkstation::getWorstations($PDOdb);
     /*
 	//pre($TWorkstation,true);exit;
 	$form = new TFormCore;
