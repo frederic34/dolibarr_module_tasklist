4f6d444d7bb563e2679ce8a66d6b180c3915c06d
atm-quentin <quentin.vial-gouteyron@atm-consulting.fr>
2019-07-23 12:07:36 +0200

    FIX script bootstrap jquery

--- tasklist.php
+++ tasklist.php
@@ -36,9 +36,9 @@
 		<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="css/jquery-ui-1.10.4.custom.min.css" />
 
-        <!-- Il faut mettre le js bootstrap avant jquery sinon il y a certains bugs jquery (exemple : il n'y a plus de croix sur les dialogs) -->
-        <script src="lib/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
         <script src="js/jquery-1.9.1.min.js" type="text/javascript"></script>
+        <!-- Il faut mettre le js bootstrap avant jquery ui sinon il y a certains bugs jquery (exemple : il n'y a plus de croix sur les dialogs) -->
+        <script src="lib/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
         <script type="text/javascript" src="js/jquery-ui-1.10.4.custom.min.js"></script>
 
 
